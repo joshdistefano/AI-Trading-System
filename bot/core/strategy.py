e399051becf049efd75820624ff5d9cbaca61755
Josh <joshua041607@gmail.com>
2026-07-18 15:50:57 -0500

    Added first trading strategy

--- bot/core/strategy.py
+++ bot/core/strategy.py
@@ -1,6 +1,23 @@
 class Strategy:
     def __init__(self):
-        self.name = "Base Strategy"
+        self.name = "Momentum Strategy"
+        self.previous_price = None
 
     def analyze(self, market_data):
-        return "NO TRADE"
+
+        if self.previous_price is None:
+            self.previous_price = market_data.price
+            return "NO TRADE"
+
+        if market_data.price > self.previous_price:
+            decision = "BUY"
+
+        elif market_data.price < self.previous_price:
+            decision = "SELL"
+
+        else:
+            decision = "NO TRADE"
+
+        self.previous_price = market_data.price
+
+        return decision
